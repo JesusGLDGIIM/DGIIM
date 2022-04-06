f30fcc10e9dd10cc057d7bdbd23678f9f311c0d5
JesusGLDGIIM <65406164+JesusGLDGIIM@users.noreply.github.com>
2022-04-06 09:41:58 +0200

    Ya no hace falta

--- PDOO/DeepSpaceRuby/lib/GameCharacter.rb
+++
@@ -1,8 +0,0 @@
-#encoding: UTF-8
-
-module Deepspace
-	module GameCharacter
-		ENEMYSTARSHIP=:enemystarship
-		SPACESTATION=:spacestation
-	end
-end
--- PDOO/DeepSpaceRuby/lib/ShotResult.rb
+++
@@ -1,9 +0,0 @@
-#encoding: UTF-8
-
-module Deepspace
-	module ShotResult
-		DONOTRESIST=:donotresist
-		RESIST=:resist
-	end
-end
-		
